7a4ef57be661f74bf7130bb00713117e70047ec8
chao liu <81363817+nullccxsy@users.noreply.github.com>
2025-09-02 01:51:38 +0800

    refactor: remove unused arrow_c_data_internal files (#203)
    - Replace individual child array management with parent array
    StartAppending/FinishElement pattern for cleaner code.
    
    ---------
    
    Co-authored-by: nullccxsy <32149055912@qq.com>

--- test/arrow_test.cc
+++ test/arrow_test.cc
@@ -28,45 +28,11 @@
 #include <arrow/util/key_value_metadata.h>
 #include <gtest/gtest.h>
 
-#include "iceberg/arrow_c_data_internal.h"
 #include "iceberg/schema.h"
 #include "iceberg/schema_internal.h"
 #include "matchers.h"
 
 namespace iceberg {
-
-TEST(ArrowCDataTest, CheckArrowSchemaAndArrayByNanoarrow) {
-  auto [schema, array] = internal::CreateExampleArrowSchemaAndArrayByNanoarrow();
-
-  auto arrow_schema = ::arrow::ImportSchema(&schema).ValueOrDie();
-  EXPECT_EQ(arrow_schema->num_fields(), 2);
-
-  auto id_field = arrow_schema->field(0);
-  EXPECT_EQ(id_field->name(), "id");
-  EXPECT_EQ(id_field->type()->id(), ::arrow::Type::INT64);
-  EXPECT_FALSE(id_field->nullable());
-
-  auto name_field = arrow_schema->field(1);
-  EXPECT_EQ(name_field->name(), "name");
-  EXPECT_EQ(name_field->type()->id(), ::arrow::Type::STRING);
-  EXPECT_TRUE(name_field->nullable());
-
-  auto arrow_record_batch = ::arrow::ImportRecordBatch(&array, arrow_schema).ValueOrDie();
-  EXPECT_EQ(arrow_record_batch->num_rows(), 3);
-  EXPECT_EQ(arrow_record_batch->num_columns(), 2);
-
-  auto id_column = arrow_record_batch->column(0);
-  EXPECT_EQ(id_column->type()->id(), ::arrow::Type::INT64);
-  EXPECT_EQ(id_column->GetScalar(0).ValueOrDie()->ToString(), "1");
-  EXPECT_EQ(id_column->GetScalar(1).ValueOrDie()->ToString(), "2");
-  EXPECT_EQ(id_column->GetScalar(2).ValueOrDie()->ToString(), "3");
-
-  auto name_column = arrow_record_batch->column(1);
-  EXPECT_EQ(name_column->type()->id(), ::arrow::Type::STRING);
-  EXPECT_EQ(name_column->GetScalar(0).ValueOrDie()->ToString(), "a");
-  EXPECT_EQ(name_column->GetScalar(1).ValueOrDie()->ToString(), "b");
-  EXPECT_EQ(name_column->GetScalar(2).ValueOrDie()->ToString(), "c");
-}
 
 struct ToArrowSchemaParam {
   std::shared_ptr<Type> iceberg_type;
